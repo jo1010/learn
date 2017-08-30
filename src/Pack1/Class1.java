cf352ffa2de476e747c9dfb86acce8fb9feb35b4
Asus <1@c.com>
2017-08-30 07:19:47 +0300

    First commit. Add Class1

--- src/Pack1/Class1.java
+++ src/Pack1/Class1.java
@@ -5,12 +5,12 @@ public class Class1 {
     Class1(int ngant){
 
     }
-}
+
 
     public static void main(String[] args) {
-        int qant;
+        int qant = 5;
+
+        Class1 gs = new Class1(qant);
 
 
-
-
-    }
+    }   }
